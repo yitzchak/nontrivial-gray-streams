2f00472c00defb2c493b8bf66cc205735f2ea8b7
Tarn W. Burton <twburton@gmail.com>
2023-11-20 05:57:34 -0500

    Add some Allegro fixes

--- code/streams.lisp
+++ code/streams.lisp
@@ -14,3 +14,15 @@
 #+lispworks
 (setf (fdefinition 'nontrivial-gray-streams:stream-line-length)
       #'stream-output-width)
+
+#+allegro
+(with-compilation-unit (:override t)
+  ;; Allegro CL is missing this default method
+  (defmethod stream-read-char-no-hang
+      ((stream fundamental-character-input-stream))
+    (stream-read-char stream))
+
+  ;; Allegro CL default method is incorrect
+  (defmethod stream-start-line-p
+      ((stream fundamental-character-output-stream))
+    (equal (stream-line-column stream) 0)))
